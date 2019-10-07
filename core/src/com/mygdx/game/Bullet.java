3a4d4043ddd585daf72f6993551602971445ee01
I400156 <d.schoenmakers@student.fontys.nl>
2019-10-07 10:59:08 +0200

    initial commit

---
+++ core/src/com/mygdx/game/Bullet.java
@@ -0,0 +1,11 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.graphics.Texture;
+
+public class Bullet {
+
+    public static final int SPEED = 300;
+    private static Texture texture;
+
+    float x, y;
+}
---
+++ tests/src/test/java/Tets.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertTrue;
+
+public class Tets {
+
+    @Test
+    public void Test1()
+    {
+        assertTrue(true);
+    }
+}
